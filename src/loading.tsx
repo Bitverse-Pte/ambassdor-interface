39faa5ac0d2b7c31acbe399a5518fda7a06fd9f3
roderick <roderick.lu@davionlabs.com>
2022-09-29 17:39:54 +0800

    update

--- src/loading.tsx
+++ src/loading.tsx
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 
 const Container = styled.div`
+  @keyframes loadingFade {
+    0% {
+      opacity: 0;
+    }
+    /* 50% {
+      opacity: 0.8;
+    } */
+    100% {
+      opacity: 1;
+    }
+  }
+
   position: fixed;
   background-color: #05050e;
   width: 100vw;
@@ -11,10 +23,51 @@ const Container = styled.div`
   right: 0;
   img {
     position: absolute;
-    top: 50%;
+    top: 44%;
     left: 50%;
     transform: translate(-50%, -50%);
     width: 150px;
+  }
+  .loading {
+    position: absolute;
+    top: 56%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    /* width: 150px; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid #ffffff;
+    border-radius: 7px;
+    padding: 5px;
+    .loading-dot {
+      float: left;
+      width: 4px;
+      height: 11px;
+      margin: 0 2px;
+      background: #01ebc8;
+
+      border-radius: 1px;
+
+      opacity: 0;
+      animation: loadingFade 4s infinite;
+    }
+  }
+  .content {
+    position: absolute;
+    top: 58%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin-top: 16px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 20px;
+    /* identical to box height, or 125% */
+
+    text-align: center;
+    text-transform: capitalize;
+
+    color: #ffffff;
   }
 `;
 
@@ -22,6 +75,19 @@ const Loading = () => {
   return (
     <Container>
       <img src={require("@/assets/walking.gif")} />
+
+      <div className="loading">
+        {// animation-delay
+        new Array(40).fill(1).map((i, index) => (
+          <div
+            key={index}
+            style={{ animationDelay: index * 0.1 + "s" }}
+            className="loading-dot"
+          />
+        ))}
+      </div>
+      <div className="content">Loading...</div>
+
     </Container>
   );
 };
