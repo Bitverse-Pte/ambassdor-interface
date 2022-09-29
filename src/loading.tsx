64322f9b7098c52b1bce3402adee76fad5aabc7a
roderick <roderick.lu@davionlabs.com>
2022-09-29 18:50:36 +0800

    update

--- src/loading.tsx
+++ src/loading.tsx
@@ -13,6 +13,7 @@ const Container = styled.div`
     }
   }
 
+  z-index: 23;
   position: fixed;
   background-color: #05050e;
   width: 100vw;
@@ -21,25 +22,26 @@ const Container = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
-  img {
+  .container {
     position: absolute;
-    top: 44%;
+    top: 45%;
     left: 50%;
     transform: translate(-50%, -50%);
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  img {
     width: 150px;
   }
   .loading {
-    position: absolute;
-    top: 56%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    /* width: 150px; */
+    margin-top: 16px;
     display: flex;
     justify-content: center;
     align-items: center;
     border: 2px solid #ffffff;
     border-radius: 7px;
-    padding: 5px;
+    padding: 5px 8px 5px 5px;
     .loading-dot {
       float: left;
       width: 4px;
@@ -54,11 +56,6 @@ const Container = styled.div`
     }
   }
   .content {
-    position: absolute;
-    top: 58%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    margin-top: 16px;
     font-weight: 500;
     font-size: 16px;
     line-height: 20px;
@@ -69,25 +66,52 @@ const Container = styled.div`
 
     color: #ffffff;
   }
+
+  .progress {
+    margin-top: 16px;
+    margin-bottom: 16px;
+    width: 330px;
+    height: 12px;
+    border: 2px solid #ffffff;
+    border-radius: 7px;
+    padding: 6px;
+    background: repeating-linear-gradient(
+        90deg,
+        #000 0 2px,
+        #01ebc8 0 5px
+      )
+      0/0% no-repeat content-box content-box;
+    position: relative;
+    animation: p4 6s infinite steps(100);
+  }
+
+  @keyframes p4 {
+    100% {
+      background-size: 120%;
+    }
+  }
+
 `;
 
 const Loading = () => {
   return (
     <Container>
-      <img src={require("@/assets/walking.gif")} />
+      <div className="container">
+        <img src={require("@/assets/walking.gif")} />
+        <div className="progress" />
 
-      <div className="loading">
-        {// animation-delay
-        new Array(40).fill(1).map((i, index) => (
-          <div
-            key={index}
-            style={{ animationDelay: index * 0.1 + "s" }}
-            className="loading-dot"
-          />
-        ))}
+        {/* <div className="loading"> */}
+        {/* {
+          new Array(40).fill(1).map((i, index) => (
+            <div
+              key={index}
+              style={{ animationDelay: index * 0.1 + "s" }}
+              className="loading-dot"
+            />
+          ))} */}
+        {/* </div> */}
+        <div className="content">Loading...</div>
       </div>
-      <div className="content">Loading...</div>
-
     </Container>
   );
 };
